0e1a11a9eb334d5a603a1ca76ecf94ac07e450ec
drun16 <drun16info@gmail.com>
2025-09-22 12:27:00 +0530

    Shared editor

--- server.js
+++ server.js
@@ -11,21 +11,28 @@ const server = http.createServer(app); // Create an HTTP server from our Express
 // We attach it to our existing HTTP server.
 const wss = new WebSocket.Server({ server });
 
+function broadcast(message, sender_ws) {
+    wss.clients.forEach((client) => {
+        //check client is not sender and is ready to recieve messages
+        if (client !== sender_ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
 // 3. Define what happens on a new connection
 wss.on('connection', (ws) => {
   // This function runs every time a new client connects.
   // The 'ws' object represents the unique connection to that one client.
-  console.log('A new client has connected!');
+  console.log('client connected!');
 
   // 4. Define what happens when a message is received from this client
   ws.on('message', (message) => {
     // The 'message' is received as a Buffer, so we convert it to a string.
     const messageStr = message.toString();
-    console.log(`Received message: "${messageStr}"`);
+    console.log(`Received : ${messageStr.substring(0,50)}....`);
 
-    // 5. Echo the message back to the sender
-    ws.send(`You sent: ${messageStr}`);
-    console.log(`Echoed message back to the client.`);
+    broadcast(messageStr, ws);
   });
 
   // 6. Define what happens when this client disconnects
